3f62ae988249afc054d52c01502ee0ab1bb4627d
Dennis Erazo <dennis.erazo96@gmail.com>
2026-08-07 14:21:41 -0600

    add: mejoras del SEO del sitio y error de responsive en footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://editorial.unag.edu.hn',
   adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
